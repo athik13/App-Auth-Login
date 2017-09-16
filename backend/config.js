15e79f9627bf94417e799a4a1066f569d7d42c93
athik13 <athik.13@gmail.com>
2017-09-16 12:55:32 +0500

    First Commit

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '521627748180895',
+  clientSecret: '89c3198c4139c8c4f6f91bf159146ed0',
+  callbackURL: 'http://artsylens.co:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '447059943665-v5equ2gnf0rbt8l4kt5uhbak21kgtpri.apps.googleusercontent.com',
+  clientSecret: 'zOer4T4achTpKKLC7Qiphk5C',
+  callbackURL: 'http://artsylens.co:3000/auth/google/callback',
+};
